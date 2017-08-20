b5791af4a7b4ab424ada0d2960b45e055645d5c9
mkruczek <kruczek86@gmail.com>
2017-08-20 18:10:58 +0200

    add database use greendao

--- app/src/main/java/pl/michalkruczek/learn/MainActivity.java
+++ app/src/main/java/pl/michalkruczek/learn/MainActivity.java
@@ -1,13 +1,67 @@
 package pl.michalkruczek.learn;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.greenrobot.greendao.database.Database;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import pl.michalkruczek.learn.db.DaoMaster;
+import pl.michalkruczek.learn.db.DaoSession;
+import pl.michalkruczek.learn.db.Question;
+import pl.michalkruczek.learn.db.QuestionDao;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+
+    private DaoSession daoSession;
+    private QuestionDao questionDao;
+    List<Question> allQuestions;
+    List<Question> todayRepeat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = MainActivity.this;
+
+        DaoMaster.DevOpenHelper helperDB = new DaoMaster.DevOpenHelper(context, "users.db");
+        Database db = helperDB.getWritableDb();
+        daoSession = new DaoMaster(db).newSession();
+        questionDao = daoSession.getQuestionDao();
+        allQuestions = questionDao.queryBuilder().list();
+        todayRepeat = new ArrayList<>();
+
+
+
+        for (Question word : allQuestions) {
+            word.setDateOfNextRepeat();
+        }
+
+
+        for (Question word : allQuestions) {
+            Date today = new Date();
+
+            DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+
+            String wordString = df.format(word.getNextRepeat());
+            String todayString = df.format(today);
+
+            if(wordString.equals(todayString)){
+                todayRepeat.add(word);
+            }
+        }
+
+        for (Question word : todayRepeat) {
+            System.out.println(word);
+        }
     }
 }
---
+++ greendaogenerator/src/main/java/com/example/MyDBGenerator.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Entity;
+import org.greenrobot.greendao.generator.Property;
+import org.greenrobot.greendao.generator.Schema;
+
+public class MyDBGenerator {
+
+
+    public static void main(String[] args) {
+
+        Schema schema = new Schema(6, "pl.michalkruczek.learn.db");
+        schema.enableKeepSectionsByDefault();
+
+        addTables(schema);
+
+        try {
+            new DaoGenerator().generateAll(schema, "./app/src/main/java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void addTables(Schema schema){
+
+        Entity category = schema.addEntity("Category");
+        category.addIdProperty().primaryKey().autoincrement();
+        category.addStringProperty("value");
+
+        Entity question = schema.addEntity("Question");
+        question.addIdProperty().primaryKey().autoincrement();
+        Property categoryIdProperty = question.addLongProperty("categoryId").getProperty();
+        question.addToOne(category, categoryIdProperty);
+        question.addStringProperty("question");
+        question.addStringProperty("answer");
+        question.addStringProperty("describe");
+        question.addDateProperty("addDate");
+        question.addDateProperty("nextRepeat");
+        question.addIntProperty("level");
+
+
+
+    }
+
+
+
+}
